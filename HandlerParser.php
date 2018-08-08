644125f42f1f0f5149984af1128b0846444c9519
Jose <joserobleda@gmail.com>
2018-08-08 10:07:20 +0200

    use phpdocumentor/reflection-docblock

--- HandlerParser.php
+++ HandlerParser.php
@@ -2,18 +2,35 @@
 
 namespace JR\CoreDocBundle;
 
+use phpDocumentor\Reflection\DocBlockFactoryInterface;
+
 class HandlerParser
 {
     /** @var HandlerMap */
     private $map;
 
-    public function __construct(HandlerMap $map)
+    /** @var DocBlockFactoryInterface */
+    private $docBlockFactory;
+
+    public function __construct(HandlerMap $map, DocBlockFactoryInterface $docBlockFactory)
     {
         $this->map = $map;
+        $this->docBlockFactory = $docBlockFactory;
     }
 
     public function parse()
     {
-        return $this->map->getHandlers();
+        return array_map([$this, 'parseCommand'], $this->map->getHandlers());
+    }
+
+    private function parseCommand($class)
+    {
+        try {
+            $rc = new \ReflectionClass($class);
+        } catch (\ReflectionException $e) {
+            return null;
+        }
+
+        return $this->docBlockFactory->create($rc->getDocComment());
     }
 }
