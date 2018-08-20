70ca7fd969bf5fd2e083c6f1abf355e1237f073f
Jose <joserobleda@gmail.com>
2018-08-20 15:29:02 +0200

    testing message bus

--- Controller/CoreDocController.php
+++ Controller/CoreDocController.php
@@ -2,12 +2,24 @@
 
 namespace JR\CoreDocBundle\Controller;
 
+use App\MessageHandler\MyMessage;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class CoreDocController
 {
+    /** @var MessageBusInterface */
+    private $bus;
+
+    public function __construct(MessageBusInterface $bus)
+    {
+        $this->bus = $bus;
+    }
+
     public function docUiAction(): Response
     {
+        $this->bus->dispatch(new MyMessage());
+
         return new Response("Hello world!");
     }
 }
